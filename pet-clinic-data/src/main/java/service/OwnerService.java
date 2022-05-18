e7396639dccc16daa0976e56c829a936a6f4ab42
sherfallak <sh.fallak@gmail.com>
2022-05-19 01:25:08 +0200

    create interfaces for service. closes #5.

---
+++ pet-clinic-data/src/main/java/service/OwnerService.java
@@ -0,0 +1,15 @@
+package service;
+
+import fallak.skylionpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findById(Long id);
+
+    Owner findByLastName(String lastName);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/service/PetService.java
@@ -0,0 +1,13 @@
+package service;
+
+import fallak.skylionpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
