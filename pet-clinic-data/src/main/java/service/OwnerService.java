b197662054f38558bfb38991ea50b2deb54af4b1
sherfallak <sh.fallak@gmail.com>
2022-05-27 07:09:57 +0200

    refactoring service to common interface. closes #12.

--- pet-clinic-data/src/main/java/service/OwnerService.java
+++ pet-clinic-data/src/main/java/service/OwnerService.java
@@ -2,14 +2,9 @@ package service;
 
 import fallak.skylionpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Owner findById(Long id);
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner save(Owner owner);
 
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/service/VetService.java
+++ pet-clinic-data/src/main/java/service/VetService.java
@@ -2,13 +2,6 @@ package service;
 
 import fallak.skylionpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
